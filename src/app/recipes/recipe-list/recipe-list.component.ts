8ba30d108e5510a021188df156ffa009f447aeb5
McMullan <benjamin.mcmullan@iao.fraunhofer.de>
2017-05-30 14:51:17 +0200

    first

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "app/recipes/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [new Recipe("Test Recipe", "Test Desc", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg"),
+   new Recipe("Test Recipe 2", "Test Desc", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-08.jpg")];
 
   constructor() { }
 
